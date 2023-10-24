a0d1d8eed46058ab086cd2b3e2a555704a136cef
maliksaqibahmad <maliksaqib7628@gmail.com>
2023-10-24 11:30:12 +0500

    ...

--- main.js
+++ main.js
@@ -114,6 +114,6 @@ const sr = ScrollReveal({
   reset: true
 })
 
-sr.reveal(`.home__perfil, .about__image, .contact__mail, .certs__card`, {origin: 'right'})
+sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin: 'right'})
 sr.reveal(`.home__name, .home__info, .about__container, .section__title-1, .about__info, .contact__social, .contact__data`, {origin: 'left'})
-sr.reveal(`.services__card, .projects__card`, {interval: 100})
+sr.reveal(`.services__card, .certs__card, .projects__card`, {interval: 100})
